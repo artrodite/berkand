f0634ea38d2d958ab93d4682ec98eb25c124a20f
Enes Kaya <eneskaya5261@gmail.com>
2023-04-25 18:50:42 +0300

    update(navbar): responsive

--- src/components/References.js
+++ src/components/References.js
@@ -17,7 +17,7 @@ export default function References() {
         return obj
     }
     return (
-        <section className="px-60 py-44">
+        <section className="px-0 md:py-44 py-16 container md:mx-auto mx-7 w-auto">
             <div className="flex mb-16">
                 <span className="font-normal text-2xl">Referanslarımız</span>
                 <img className="ml-2" src="/assets/icons/union.svg" alt=""/>
@@ -25,129 +25,6 @@ export default function References() {
             <div className="grid grid-cols-2 lg:grid-cols-5 gap-20">
                 {createReferences()}
             </div>
-            {/*<div className="flex flex-col mt-20">*/}
-            {/*  <div className="flex justify-between">*/}
-            {/*    <div className="w-28 h-28 flex justify-center items-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/1.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/2.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/3.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/4.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/5.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*  </div>*/}
-            {/*  <div className="flex justify-between">*/}
-            {/*    <div className="w-28 h-28 flex justify-center items-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/6.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/7.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/8.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/10.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*  </div>*/}
-            {/*  <div className="flex justify-between">*/}
-            {/*    <div className="w-28 h-28 flex justify-center items-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/11.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/12.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/13.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/14.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mb-32">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/15.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*  </div>*/}
-            {/*  <div className="flex">*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mr-[216px]">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/16.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*    <div className="w-28 h-28 object-cover flex justify-center mr-">*/}
-            {/*      <img*/}
-            {/*        className="object-contain"*/}
-            {/*        src="/assets/brands/17.png"*/}
-            {/*        alt=""*/}
-            {/*      />*/}
-            {/*    </div>*/}
-            {/*  </div>*/}
-            {/*</div>*/}
         </section>
     );
 }
