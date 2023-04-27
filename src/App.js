8586bc86112aa8b6eb95546895fe2cda5076c878
Enes Kaya <eneskaya5261@gmail.com>
2023-04-27 23:16:15 +0300

    update(App.js): add Routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
+import Product from "./pages/Product";
 
 function App() {
   return (
-    <div className="App font-[Roboto]">
+    <Router className="App font-[Roboto]">
       <Navbar />
-      <Contact />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
